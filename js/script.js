c3105ee71631bf1ebf6bfc6d0152fcd0bfec96fd
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-29 12:48:54 +0100

    add style to warnings

--- js/script.js
+++ js/script.js
@@ -11,14 +11,19 @@ function validateForm() {
 
 	if (userName === '') {
 		document.getElementById('infoName').innerHTML = 'Wpisz imię i nazwisko.';
+		document.getElementById('infoName').classList.add('active');
 	} else if (userName == 'Jan Kowalski') {
 		document.getElementById('infoName').innerHTML = 'Użytkowanik już istnieje.';
+		document.getElementById('infoName').classList.add('active');
 	} else if (email === '') {
 		document.getElementById('infoMail').innerHTML = 'Wpisz e-maila.';
+		document.getElementById('infoMail').classList.add('active');
 	} else if (country === '') {
 		document.getElementById('infoCountry').innerHTML = 'Wybierz kraj.';
+		document.getElementById('infoCountry').classList.add('active');
 	} else if (approval == false) {
 		document.getElementById('infoApproval').innerHTML = 'Trzeba wyrazić zgodę.';
+		document.getElementById('infoApproval').classList.add('active');
 	} else {
 		document.querySelector('.modal-wrap').classList.add('active');
 		document.querySelector('.mainView').classList.add('hidden');		
