38b47d4b1e07740e3a651e0d4f01428e0208a1b6
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-29 02:49:03 +0100

    check that field is filled

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 const registerBtn = document.getElementById('register-btn');
 const letsGo = document.getElementById('letsGo');
 
+
+
 registerBtn.addEventListener('click', function() {
-	document.querySelector('.modal-wrap').classList.add('active');
-	document.querySelector('.mainView').classList.add('hidden');
+	let x = document.forms['myForm']['country'].value;
+	let y = document.getElementById('checkbox').checked;
+
+	if (x !== "") {
+		if (y == true) {
+			document.querySelector('.modal-wrap').classList.add('active');
+			document.querySelector('.mainView').classList.add('hidden');
+		}	
+	}
 })
 
 letsGo.addEventListener('click' ,function() {
