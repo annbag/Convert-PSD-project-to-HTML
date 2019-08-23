d7d06b92da78f4739c69b586289719cdf29eb775
Anna Ambroziak <ambroziak_anna@wp.pl>
2019-08-23 10:53:24 +0200

    fix rwd

--- js/script.js
+++ js/script.js
@@ -2,37 +2,36 @@ const registerBtn = document.getElementById('register-btn');
 const letsGo = document.getElementById('letsGo');
 
 
-
 function validateForm() {
-	const userName = document.forms['myForm']['userName'].value;
-	const email = document.forms['myForm']['email'].value;
-	const country = document.forms['myForm']['country'].value;
-	const approval = document.getElementById('checkbox').checked;
+    const userName = document.forms['myForm']['userName'].value;
+    const email = document.forms['myForm']['email'].value;
+    const country = document.forms['myForm']['country'].value;
+    const approval = document.getElementById('checkbox').checked;
 
-	if (userName === '') {
-		document.getElementById('infoName').innerHTML = 'Wpisz imię i nazwisko.';
-		document.getElementById('infoName').classList.add('active');
-	} else if (userName == 'Jan Kowalski') {
-		document.getElementById('infoName').innerHTML = 'Użytkowanik już istnieje.';
-		document.getElementById('infoName').classList.add('active');
-	} else if (email === '') {
-		document.getElementById('infoMail').innerHTML = 'Wpisz e-maila.';
-		document.getElementById('infoMail').classList.add('active');
-	} else if (country === '') {
-		document.getElementById('infoCountry').innerHTML = 'Wybierz kraj.';
-		document.getElementById('infoCountry').classList.add('active');
-	} else if (approval == false) {
-		document.getElementById('infoApproval').innerHTML = 'Trzeba wyrazić zgodę.';
-		document.getElementById('infoApproval').classList.add('active');
-	} else {
-		document.querySelector('.modal-wrap').classList.add('active');
-		document.querySelector('.mainView').classList.add('hidden');		
-	}
+    if (userName === '') {
+        document.getElementById('infoName').innerHTML = 'Wpisz imię i nazwisko.';
+        document.getElementById('infoName').classList.add('active');
+    } else if (userName == 'Jan Kowalski') {
+        document.getElementById('infoName').innerHTML = 'Użytkowanik już istnieje.';
+        document.getElementById('infoName').classList.add('active');
+    } else if (email === '') {
+        document.getElementById('infoMail').innerHTML = 'Wpisz e-maila.';
+        document.getElementById('infoMail').classList.add('active');
+    } else if (country === '') {
+        document.getElementById('infoCountry').innerHTML = 'Wybierz kraj.';
+        document.getElementById('infoCountry').classList.add('active');
+    } else if (approval == false) {
+        document.getElementById('infoApproval').innerHTML = 'Trzeba wyrazić zgodę.';
+        document.getElementById('infoApproval').classList.add('active');
+    } else {
+        document.querySelector('.modal-wrap').classList.add('active');
+        document.querySelector('.mainView').classList.add('hidden');
+    }
 }
 
-registerBtn.addEventListener('click', validateForm );
+registerBtn.addEventListener('click', validateForm);
 
-letsGo.addEventListener('click' ,function() {
-	document.querySelector('.modal-wrap').classList.remove('active');
-	document.querySelector('.mainView').classList.remove('hidden');
+letsGo.addEventListener('click', function () {
+    document.querySelector('.modal-wrap').classList.remove('active');
+    document.querySelector('.mainView').classList.remove('hidden');
 })
